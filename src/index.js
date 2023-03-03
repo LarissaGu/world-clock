e63056e7ae11138bc0bed8436a0120089aabbdb0
LarissaGu <68541012+LarissaGu@users.noreply.github.com>
2023-03-03 10:47:04 +0100

    Added HTML & JS for dropdown functionality

--- src/index.js
+++ src/index.js
@@ -1,20 +1,43 @@
 function updateTime() {
   let berlinElement = document.querySelector("#berlin");
-  let berlinDateElement = berlinElement.querySelector("#berlin .date");
-  let berlinTimeElement = berlinElement.querySelector("#berlin .time");
-  let berlinTime = moment().tz("Europe/Berlin");
+  if (berlinElement) {
+    let berlinDateElement = berlinElement.querySelector("#berlin .date");
+    let berlinTimeElement = berlinElement.querySelector("#berlin .time");
+    let berlinTime = moment().tz("Europe/Berlin");
 
-  berlinDateElement.innerHTML = berlinTime.format("dddd, DD/MM/YYYY");
-  berlinTimeElement.innerHTML = berlinTime.format("H:mm:ss");
+    berlinDateElement.innerHTML = berlinTime.format("dddd, DD/MM/YYYY");
+    berlinTimeElement.innerHTML = berlinTime.format("H:mm:ss");
+  }
 
   let istanbulElement = document.querySelector("#istanbul");
-  let istanbulDateElement = istanbulElement.querySelector("#istanbul .date");
-  let istanbulTimeElement = istanbulElement.querySelector("#istanbul .time");
-  let istanbulTime = moment().tz("Asia/Istanbul");
+  if (istanbulElement) {
+    let istanbulDateElement = istanbulElement.querySelector("#istanbul .date");
+    let istanbulTimeElement = istanbulElement.querySelector("#istanbul .time");
+    let istanbulTime = moment().tz("Asia/Istanbul");
 
-  istanbulDateElement.innerHTML = istanbulTime.format("dddd, DD/MM/YYYY");
-  istanbulTimeElement.innerHTML = istanbulTime.format("H:mm:ss");
+    istanbulDateElement.innerHTML = istanbulTime.format("dddd, DD/MM/YYYY");
+    istanbulTimeElement.innerHTML = istanbulTime.format("H:mm:ss");
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.split(`/`)[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let cityElement = document.querySelector("#cities");
+  cityElement.innerHTML = `<div class="city">
+          <div>
+            <h2>${cityName}</h2>
+
+            <div class="date">${cityTime.format("dddd, DD/MM/YYYY")}</div>
+          </div>
+
+          <div class="time">${cityTime.format("H:mm:ss")}</div>
+        </div>`;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
